74550c1202c40b01297f99bbf91baea9f544343e
Kevin Loughlin <kevinloughlin@college.harvard.edu>
2015-08-25 15:53:24 -0400

    Added tutorial videos

--- Develop/LASAD-Client/src/lasad/gwt/client/ui/workspace/tabs/TutorialVideosTab.java
+++ Develop/LASAD-Client/src/lasad/gwt/client/ui/workspace/tabs/TutorialVideosTab.java
@@ -11,11 +11,13 @@ public class TutorialVideosTab extends TabItem
 	private final int VID_HEIGHT = 360;
 	private final int VID_WIDTH = 640;
 	private final String VID_PATH = "resources/videos/";
+	private LinkedHashMap<String, String> titlesFilenames;
 
 	private static TutorialVideosTab instance = null;
 
 	private TutorialVideosTab()
 	{
+		titlesFilenames = new LinkedHashMap<String, String>();
 		this.setText("Tutorial Videos");
 		this.setClosable(true);
 		this.setScrollMode(Scroll.AUTO);
@@ -26,12 +28,22 @@ public class TutorialVideosTab extends TabItem
 		// TODO This text should be loaded from the server.
 		HTML page = new HTML();
 		StringBuilder buffer = new StringBuilder("<div align=\"center\"><img src=\"resources/images/lasad-tutorial-videos.png\" border=\"0\"><br><style>a.vidlist:link {color:blue; background-color:transparent; text-decoration:underline} a.vidlist:visited {color:blue; background-color:transparent; text-decoration:underline} a.vidlist:hover   {color:purple; background-color:transparent; text-decoration:underline} a.vidlist:active  {color:blue; background-color:transparent; text-decoration:underline}</style>");
-		LinkedHashMap<String, String> titlesFilenames = new LinkedHashMap<String, String>();
-		titlesFilenames.put("Logging in and Joining Maps", VID_PATH + "login.mp4");
-		titlesFilenames.put("Workspace Tour", VID_PATH + "workspace.mp4");
+		
+		putEntry("Logging in and Joining Maps", "login.mp4");
+		putEntry("Workspace Tour", "workspace.mp4");
+		putEntry("Creating Contributions", "contributions.mp4");
+		putEntry("Creating Relations", "relations.mp4");
+		putEntry("Deleting Contributions & Relations", "deleting.mp4");
+		putEntry("Linked Connections", "linked.mp4");
+		putEntry("Finding & Centering Contributions", "centering.mp4");
+		putEntry("Auto Organization", "organization.mp4");
+		putEntry("Capturing Screenshots", "screenshots.mp4");
+		putEntry("Importing & Exporting Maps", "importing.mp4");
+		putEntry("Closing Maps and Logging Out", "logout.mp4");
+
 		for (String title : titlesFilenames.keySet())
 		{
-			buffer.append("<a class=\"vidlist\" href=\"#" + title + "\">"+title+"</a><br>");
+			buffer.append("<a class=\"vidlist\" href=\"#" + title + "\" style=\"font-family:Georgia, serif\">"+title+"</a><br>");
 		}
 
 		for (String title : titlesFilenames.keySet())
@@ -39,7 +51,7 @@ public class TutorialVideosTab extends TabItem
 			buffer.append(vidString(title, titlesFilenames.get(title)));
 		}
 
-		buffer.append("<font color=#000000></div></font>");
+		buffer.append("<br><font color=#000000></div></font>");
 		page.setHTML(buffer.toString());
 		this.add(page);
 	}
@@ -47,6 +59,11 @@ public class TutorialVideosTab extends TabItem
 	private String vidString(String title, String filename)
 	{
 		return "<br><h2 id=\""+title+"\" style=\"font-family:Georgia, serif\">"+title+"</h2><video width=\""+VID_WIDTH+"\" height=\""+VID_HEIGHT+"\" style=\"border:5px solid black\" controls><source src=\""+filename+"\" type=\"video/mp4\">Your browser does not support the video tag.</video><br>";
+	}
+
+	private void putEntry(String title, String filename)
+	{
+		titlesFilenames.put(title, VID_PATH + filename);
 	}
 
 	public static TutorialVideosTab getInstance()
